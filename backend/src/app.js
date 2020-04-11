c08cbfe517bcb23990d33446b7620b997b60d2cf
Amaechi Chuks <amaechichuks2000@yahoo.com>
2020-04-11 03:27:47 +0100

    feature(authController): build out user CRUD
    - create user login controller
    - create user Signup controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,16 @@ import cors from "cors";
 import express from "express";
 import createError from "http-errors";
 import morganBody from "morgan-body";
-import api from "./api";
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import trimmer from './helpers/trimmer';
+// import api from "./api";
 import mongoManager from "./mongo";
-//require("dotenv").config();
+import router from './controllers/users'
+
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //ENVIRONMENT VARIABLES GO HERE
 const NODE_ENV = process.env.NODE_ENV;
@@ -26,12 +31,33 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+//trimmer middleware
+app.use(trimmer);
+
+// parse application/json
+app.use(bodyParser.json())
 
 mongoManager();
 
+//User environment variables
+dotenv.config();
+
+//set a base URl for the application
+const baseUrl = '/api/v1'
+
 // use all routes exported from the routes folder
-app.use("/api", api);
+// app.use("/api", api);
+//Index Route ---/api
+//Get Welcome Message
+app.get('/', (req, res) => {
+  res.send('Welcome v18-bears-team-04');
+});
+
+//Routers
+app.use(`${baseUrl}`, router);
 
 // used to catch any routes not found
 app.use((req, _, next) => {
@@ -46,4 +72,7 @@ const GlobalErrorHandler = ({ status, message }, _req, res, _next) => {
 
 app.use(GlobalErrorHandler);
 
+app.listen(port, ()=>{
+console.log(`Application is running on Port ${port}`)
+})
 module.exports = app;
