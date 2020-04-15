5091abc46cacfcf6d6c3c6745cb711830f8a8b50
josno <jnsenoren@gmail.com>
2020-04-15 15:00:02 +0400

    added assignments fetch calls to api-service

--- education-app/src/services/api-services.js
+++ education-app/src/services/api-services.js
@@ -38,6 +38,41 @@ const ApiService = {
 			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
 		);
 	},
+	getAssignments() {
+		return fetch(`${config.API_ENDPOINT}/assignment`).then((res) =>
+			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+		);
+	},
+	addAssignment(obj) {
+		return fetch(`${config.API_ENDPOINT}/assignment`, {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(obj),
+		}).then((res) =>
+			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+		);
+	},
+	getAssignmentById(assignmentId) {
+		return fetch(
+			`${config.API_ENDPOINT}/assignment/${assignmentId}`
+		).then((res) =>
+			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+		);
+	},
+	updateAssignmentById(obj, assignmentId) {
+		return fetch(`${config.API_ENDPOINT}/assignment/${assignmentId}`, {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(obj),
+		}).then((res) =>
+			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+		);
+	},
+	// router.delete("/:assignmentId", deleteSingleAssignmentById);
 };
 
 export default ApiService;
