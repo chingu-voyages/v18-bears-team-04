7357c7515a5cd048652915f18622e26eb944dd4c
Amaechi Chuks <amaechichuks2000@yahoo.com>
2020-04-17 17:11:01 +0100

    feature(Notifications): Build out notification CRUD operation
    - add notifications
    - get notification by assignemnt Id
    - delete notifications
    
    [Delivered]

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,6 +3,7 @@ import UserController from "../controllers/users";
 import ClassController from "../controllers/classes";
 import AssignmentController from "../controllers/assignments";
 import GradeController from '../controllers/grades';
+import NotificationController from '../controllers/notifications';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.get("/", (_, res) => res.send("v18 Bears API"));
 router.use("/user", UserController);
 router.use("/class", ClassController);
 router.use("/assignment", AssignmentController);
-router.use("/grade", GradeController)
+router.use("/grade", GradeController);
+router.use("/notification", NotificationController);
 
 export default router;
