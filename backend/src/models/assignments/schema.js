c29a752cdc778b12b52d72b6c9510e3ee0a408a1
Liam Banga <liam205@outlook.com>
2020-04-23 00:09:47 +1000

    Added API to Upload Teachers Assignment Doc

--- backend/src/models/assignments/schema.js
+++ backend/src/models/assignments/schema.js
@@ -1,50 +1,52 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const assignmentSchema = new Schema({
-    classId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Class",
+  classId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+  },
+  assignmentResults: {
+    type: String,
+    enum: ["A", "B", "C", "D", "E", "F"],
+  },
+  studentFeedback: {
+    type: String,
+    default: null,
+  },
+  teacherFeedback: {
+    type: String,
+    default: null,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+  },
+  startDate: {
+    type: Date,
+    default: null,
+  },
+  dueDate: {
+    type: Date,
+    default: null,
+  },
+  submitted: {
+    type: Boolean,
+    default: false,
+  },
+  status: {
+    type: Date,
+    default: null,
+  },
+  teacherDocLink: [
+    {
+      type: String,
     },
-    assignmentResults: {
-        type: String,
-        enum: ["A", "B", "C", "D", "E", "F"],
-    },
-    studentFeedback: {
-        type: String,
-        default: null
-    },
-    teacherFeedback: {
-        type: String,
-        default: null
-    },
-      title: {
-          type: String,
-          required: true,
-         
-      },
-      instructions:{
-          type: String,
-          required: true,
-      },
-      startDate: {
-          type: Date,
-          default: null
-      },
-      dueDate: {
-          type: Date,
-          default: null
-      }, 
-      submitted: {
-          type: Boolean,
-          default: false
-      },
-      status: {
-        type: Date,
-        default: null
-      }
+  ],
 });
 
 const Assignment = mongoose.model("Assignment", assignmentSchema);
 export default Assignment;
-
