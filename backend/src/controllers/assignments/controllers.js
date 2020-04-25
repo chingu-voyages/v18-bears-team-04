d094385ed7761b7b18bc4994a471ee8fdae82b09
Liam Banga <liam205@outlook.com>
2020-04-25 18:37:03 +1000

    Update the assignment schema and controllers

--- backend/src/controllers/assignments/controllers.js
+++ backend/src/controllers/assignments/controllers.js
@@ -4,24 +4,35 @@ import User, { userRole } from "../../models/users";
 import Class from "../../models/classes";
 import notifications from "../../helper/notifications";
 
+//Create an assignment. Req.body must include classId, title,
+//dueDate. Date must be a JSON Date. Example: '2020-05-26T07:56:00.123Z'
 export const createAssignment = async (req, res, next) => {
   try {
-    const { teacherName, classId } = req.body;
-
-    //User validation
-    const user = await User.findOne({ userName: teacherName });
-    if (!user) throw createError(404, `Teacher ${teacherName} not found`);
-    //Authorization Validation
-    if (user.role != userRole.TEACHER) {
-      throw createError(404, `User ${teacherName} is not a Teacher`);
-    }
-
+    const { classId } = req.body;
     //validate if class exist
     const existingClass = await Class.findById(classId);
     if (!existingClass)
-      throw createError(404, `Claas ${classId} dose not exist`);
+      throw createError(404, `Claas ${classId} does not exist`);
+
     //create an new Assignment
     const assignment = await Assignment.create(req.body);
+
+    //Add all students that belong to the class, to the Assignment Results object
+    //Array of Students
+    const studentIds = existingClass.studentIds;
+
+    studentIds.foreach(async (studentId) => {
+      await Assignment.findOneAndUpdate(
+        { _id: assignment._id },
+        {
+          $push: {
+            assignmentResults: {
+              studentId,
+            },
+          },
+        }
+      );
+    });
 
     //Send both email and in-app notification
     await notifications.sendStudentsNotification(
@@ -38,6 +49,51 @@ export const createAssignment = async (req, res, next) => {
   }
 };
 
+//Req.body must include the studentId
+export const addStudentToAssignment = async (req, res, next) => {
+  try {
+    const { assignmentId } = req.params;
+    const { studentId } = req.body;
+
+    //check if assignment exists in the db
+    const existingAssignment = await Assignment.findById(assignmentId);
+    if (!existingAssignment)
+      throw createError(
+        404,
+        `Assignment with id ${assignmentId} does not exist`
+      );
+
+    //check if user exists in the db
+    const user = await User.findOne({ _id: studentId });
+    if (!user)
+      throw createError(404, `Student with id  (${studentId}) not Found`);
+
+    //Check if User is a Teacher
+    if (user.role != userRole.STUDENT)
+      throw createError(404, `User with id (${studentId}) is not a Student`);
+
+    //Add StudentId to Class Table's studentIds Array
+    const newAssignment = await Assignment.findOneAndUpdate(
+      { _id: assignmentId },
+      {
+        $push: {
+          assignmentResults: {
+            studentId,
+          },
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      msg: "Student Added to the Assignment",
+      newAssignment,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const getAllAssignment = async (_req, res, next) => {
   try {
     const assignments = await Assignment.find().lean();
@@ -47,21 +103,21 @@ export const getAllAssignment = async (_req, res, next) => {
     next(err);
   }
 };
-export const getAllAssignmentByStatus = async (req, res, next) => {
-  try {
-    const { status } = req.params;
-    let assignments = await Assignment.findOne({ submitted: status });
-    if (!assignments) throw createError(404, `Assignment ${status} not found`);
-    if (assignments.submitted === true) {
-      return res.status(200).json(assignments);
-    }
-    if (assignments.submitted === false) {
-      return res.status(200).json(assignments);
-    }
-  } catch (err) {
-    next(err);
-  }
-};
+// export const getAllAssignmentByStatus = async (req, res, next) => {
+//   try {
+//     const { status } = req.params;
+//     let assignments = await Assignment.findOne({ submitted: status });
+//     if (!assignments) throw createError(404, `Assignment ${status} not found`);
+//     if (assignments.submitted === true) {
+//       return res.status(200).json(assignments);
+//     }
+//     if (assignments.submitted === false) {
+//       return res.status(200).json(assignments);
+//     }
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const updateAssignment = async (req, res, next) => {
   try {
---
+++ backend/src/models/assignmentResults/schema.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+export const status = {
+  NOT_SUBMITTED: "NOT SUBMITTED",
+  SUBMITTED: "SUBMITTED",
+  GRADED: "GRADED",
+};
+
+export const assignmentResultsSchema = new Schema({
+  studentId: {
+    type: String,
+    ref: "Student",
+    //required: true,
+    //unique: true,
+  },
+
+  status: {
+    type: String,
+    enum: [status.NOT_SUBMITTED, status.SUBMITTED, status.GRADED],
+    default: status.NOT_SUBMITTED,
+  },
+
+  grade: {
+    type: Number,
+    min: 0,
+    max: 100,
+  },
+  studentDocLink: {
+    type: String,
+  },
+  studentAnswers: {
+    type: String,
+  },
+  submittedOnDate: {
+    type: Date,
+  },
+  studentFeedback: {
+    type: String,
+  },
+  teacherFeedback: {
+    type: String,
+  },
+});
+
+const AssignmentResults = mongoose.model(
+  "AssignmentResults",
+  assignmentResultsSchema
+);
+export default AssignmentResults;
