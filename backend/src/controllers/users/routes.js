b1a785a77cb3250f225a7d2263de9a2156f3097d
Liam Banga <liam205@outlook.com>
2020-04-25 19:12:42 +1000

    Update the Get User Controller and Route

--- backend/src/controllers/users/routes.js
+++ backend/src/controllers/users/routes.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import {
   getEveryUser,
-  getUserByName,
+  getUserByParams,
   createUser,
   deleteUserById,
 } from "./controllers";
 const router = Router();
 
-router.get("/", getEveryUser);
-router.get("/name/:userName", getUserByName);
+router.get("/all", getEveryUser);
+
+//Get User Based on UserName or UserId
+//Examples requests:1) http://localhost:5000/api/user/?userName=lewis
+//2) http://localhost:5000/api/user/?userId=5ea3f5e21fc0296afd2fdaa4
+router.get("", getUserByParams);
+
 router.post("", createUser);
+
 router.delete("/id/:userId", deleteUserById);
 
 export default router;
