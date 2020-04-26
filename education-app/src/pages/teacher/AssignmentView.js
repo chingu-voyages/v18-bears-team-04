c086feb969a8c87ec85ed30319ccae834a18704e
josno <jnsenoren@gmail.com>
2020-04-27 00:03:54 +0400

    Fixed submitted assignment view (teacher)

--- education-app/src/pages/teacher/AssignmentView.js
+++ education-app/src/pages/teacher/AssignmentView.js
@@ -36,12 +36,9 @@ const AssignmentView = (props) => {
 		});
 	};
 
-	useEffect(
-		(props) => {
-			getAssignmentInfo(props);
-		},
-		[props]
-	);
+	useEffect(() => {
+		getAssignmentInfo(props);
+	}, [props]);
 
 	return (
 		<>
