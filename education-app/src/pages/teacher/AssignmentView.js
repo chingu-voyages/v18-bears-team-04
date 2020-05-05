58d50a0c75fe3ae3217217ed355c72c0780961db
josno <jnsenoren@gmail.com>
2020-05-06 03:11:09 +0400

    Refactored for updated teacher feedback submission

--- education-app/src/pages/teacher/AssignmentView.js
+++ education-app/src/pages/teacher/AssignmentView.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import config from "../../config";
 import moment from "moment";
@@ -10,6 +11,8 @@ import styled from "styled-components";
 
 const AssignmentView = (props) => {
 	const [assignmentInfo, setInfo] = useState(null);
+	const [fileUrl, setFileUrl] = useState([]);
+	const [assignmentId, setAssignmentId] = useState(null);
 	const [error, setError] = useState(null);
 
 	const getAssignmentInfo = (props) => {
@@ -19,14 +22,22 @@ const AssignmentView = (props) => {
 					a.assignmentResults.find(
 						(b) => b._id === props.match.params.assignmentId
 					);
+
 				const filteredAssignment = res.find((a) => filterAssignment(a));
+				setAssignmentId(filteredAssignment._id);
 				const currentAssignment = filteredAssignment.assignmentResults.find(
 					(a) => a._id === props.match.params.assignmentId
 				);
 
-				console.log(currentAssignment);
-
 				setInfo(currentAssignment);
+
+				let list = [];
+				if (currentAssignment.studentDocLink.length > 0) {
+					list = currentAssignment.studentDocLink.map(
+						(a) => config.FILE_BASE_URL + a
+					);
+				}
+				setFileUrl(list);
 			})
 
 			.catch((err) => setError({ error: err }));
@@ -36,6 +47,17 @@ const AssignmentView = (props) => {
 		getAssignmentInfo(props);
 	}, [props]);
 
+	const renderCurrentFiles =
+		fileUrl !== null
+			? fileUrl.map((a, index) => {
+					return (
+						<a key={index} className='download-btn' href={a} download>
+							Download File {index + 1}
+						</a>
+					);
+			  })
+			: null;
+
 	const handleFeedbackChange = (e) => {
 		e.preventDefault();
 		setInfo({
@@ -44,19 +66,25 @@ const AssignmentView = (props) => {
 		});
 	};
 
+	const handleSubmitFeedback = (e) => {
+		e.preventDefault();
+
+		const obj = {
+			assignmentId: assignmentId,
+			studentId: assignmentInfo.studentId,
+			teacherFeedback: assignmentInfo.teacherFeedback,
+		};
+		ApiService.addTeacherFeedback(obj)
+			.then((res) => props.history.goBack())
+			.catch((err) => setError({ error: err }));
+	};
+
 	const formatDate = () => {
 		if (assignmentInfo.submittedOnDate === null) {
 			return;
 		}
 		return moment(assignmentInfo.submittedOnDate).format("MMMM DD, YYYY");
 	};
-
-	const uploadedAssignmentURL =
-		assignmentInfo !== null &&
-		assignmentInfo.studentDocLink !== undefined &&
-		assignmentInfo.studentDocLink.length > 0
-			? config.FILE_BASE_URL + assignmentInfo.studentDocLink[0]
-			: null;
 
 	return (
 		<>
@@ -67,34 +95,32 @@ const AssignmentView = (props) => {
 					<div className='submission-container'>
 						<div className='student-submission-view-container'>
 							<h2> Student: {props.match.params.studentUsername}</h2>
-							<a className='download-btn' href={uploadedAssignmentURL} download>
-								Download File
-							</a>
+							<div className='file-downloads'> {renderCurrentFiles}</div>
 							<span className='submitted-date'>
 								Submitted on {assignmentInfo !== null && formatDate()}
 							</span>
 							<div className='submission-text'>
 								{assignmentInfo !== null && assignmentInfo.studentAnswers}
 							</div>
-							{/* Add Link to download document */}
 						</div>
 
 						<div className='feedback-container'>
 							<h2>Feedback</h2>
-							<textarea
-								className='feedback-textarea'
-								value={
-									assignmentInfo !== null ? assignmentInfo.teacherFeedback : " "
-								}
-								onChange={(e) => handleFeedbackChange(e)}
-							/>
-							<button
-								disabled={
-									assignmentInfo !== null && assignmentInfo.teacherFeedback
-								}
+							<form
+								htmlFor='teacher-feedback'
+								onSubmit={(e) => handleSubmitFeedback(e)}
 							>
-								Submit
-							</button>
+								<textarea
+									className='feedback-textarea'
+									value={
+										assignmentInfo !== null
+											? assignmentInfo.teacherFeedback
+											: " "
+									}
+									onChange={(e) => handleFeedbackChange(e)}
+								/>
+								<button>Submit</button>
+							</form>
 						</div>
 					</div>
 				</div>
@@ -135,27 +161,32 @@ const AssignmentViewStyle = styled.div`
 		margin: 40px 40px;
 		box-shadow: 2px 3px 3px 3px #a9a9a9;
 		border-radius: 10px;
+		form {
+			display: flex;
+			flex-direction: column;
+		}
 	}
 	.submission-text {
 		margin: 20px;
-		width: auto;
-		display: block;
-		height: 800px;
-		border: 1px solid #a9a9a9;
-		border-radius: 10px;
-		font-size: 2rem;
-		padding: 10px;
+    	width: auto;
+    	display: block;
+    	height: 800px;
+    	border: 1px solid #a9a9a9;
+   		font-size: 2rem;
+    	overflow-y: scroll;
+ 		padding: 10px;
 	}
 	.download-btn {
 		background-color: #00a3ff;
-		line-height: 40px;
-		text-align:center;
-		font-size: 2rem;
-		width: 200px;
-		height: 40px;
-		margin: 3px auto 3px 20px;
-		color: #ffffff;
-		border-radius: 10px;
+    	line-height: 40px;
+    	text-align: center;
+    	font-size: 2rem;
+    	width: 200px;
+    	height: 40px;
+    	margin: 3px auto 3px 20px;
+    	color: #ffffff;
+    	border-radius: 5px;
+    	padding: 5px;
 	}
 	.submitted-date{
 		font-size: 1.25rem;
@@ -186,6 +217,7 @@ const AssignmentViewStyle = styled.div`
         font-size: 1.5rem;
 }
 	}
+	
 `;
 
 export default AssignmentView;
