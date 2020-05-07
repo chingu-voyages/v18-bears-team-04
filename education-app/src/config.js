e202efd304bbdd9060495f7b4f9863ea7166630f
josno <jnsenoren@gmail.com>
2020-05-07 20:05:14 +0400

    Update config and package.json for deployment

--- education-app/src/config.js
+++ education-app/src/config.js
@@ -3,6 +3,6 @@ export default {
 	FILE_BASE_URL: process.env.REACT_APP_FILE_BASE_URL,
 	TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 	CLASS_TOKEN_KEY: process.env.REACT_APP_CLASS_KEY,
-	// API_ENDPOINT: "http://localhost:5000/api",
-	// FILE_BASE_URL: "http://localhost:5000",
+	// API_ENDPOINT: "https://localhost:5000/api",
+	// FILE_BASE_URL: "https://localhost:5000",
 };
